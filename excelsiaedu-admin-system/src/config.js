7294c7d5b35e66a48b9d438307e51a608e078243
ianho77 <ian20051102@gmail.com>
2025-08-10 17:04:53 +0800

    Switch to Render backend API - update config to use Render server

--- excelsiaedu-admin-system/src/config.js
+++ excelsiaedu-admin-system/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // 在生產環境中使用相對路徑，在開發環境中使用本地服務器
-  API_URL: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3001'
+  // 使用Render部署的後端服務器
+  API_URL: 'https://excelsiaedu-admin-system.onrender.com'
 };
 
 export default config;
